faab771e7233dd98d198f5efb0f505cfd0d26cd5
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-10-07 18:28:57 -0300

    jest

---
+++ src/Tests/produtos.test.js
@@ -0,0 +1,17 @@
+import { act, render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Produtos from '../Pages/Produtos';
+
+
+
+test('If product exist', () => {
+  render(
+    <BrowserRouter>
+      <Produtos handleCarrinho={() => {}} carrinho={[{}]}></Produtos>
+    </BrowserRouter>
+  )
+  const produtos = screen.getAllByTestId('produtos')
+  fireEvent.click(produtos[0])
+});
